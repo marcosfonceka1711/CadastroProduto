4e8359528d88ff83e55f96b5db9f14cd8ea0e69d
Marcos Roberto da Fonseca <marcosfonceka@gmail.com>
2021-10-31 19:23:31 -0300

    Api produto

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+using CadastroProduto.Interfaces;
+
+namespace CadastroProduto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllCategories(CancellationToken cancellationToken)
+        {
+            var produtos = await _produtoRepositorio.ListarTodos();
+
+            return Ok(produtos);
+        }
+    }
+}
